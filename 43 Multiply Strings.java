1793285645d6b4e46ac0438d5dc60aa7d0992f8f
orangehhh <haoliu47@gmail.com>
2019-08-07 23:28:22 -0500

    Math, String, Two big number multiplication
    Math, String, Two big numbers multiplication

---
+++ 43 Multiply Strings.java
@@ -0,0 +1,36 @@
+/*
+ * @Author: Hao Liu
+ * @Date: 2019-08-07 23:26:59
+ * @LastEditors: Hao Liu
+ * @LastEditTime: 2019-08-07 23:27:19
+ * @Description: 
+ */
+
+class Solution {
+    public String multiply(String num1, String num2) {
+        if (num1 == null || num2 == null || num1.length() == 0 || num2.length() == 0) {
+            return "";
+        }
+        if (num1.equals("0") || num2.equals("0")) {
+            return "0";
+        }
+        int m = num1.length();
+        int n = num2.length();
+        StringBuilder sb = new StringBuilder();
+        int[] digit = new int[m + n];
+        for (int i = m - 1; i >= 0; i--) {
+            for (int j = n - 1; j >= 0; j--) {
+                int mul = (num1.charAt(i) - '0') * (num2.charAt(j) - '0');
+                int sum = digit[i + j + 1] + mul;
+                digit[i + j + 1] = sum % 10;
+                digit[i + j] += sum / 10;
+            }
+        }
+        for (int i : digit) {
+            if (!(sb.length() == 0 && i == 0)) {
+                sb.append(i);
+            }
+        }
+        return sb.toString();
+    }
+}

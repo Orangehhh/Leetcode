17d6c4cd336426fe81e93170068b0ac3f393cd9c
orangehhh <haoliu47@gmail.com>
2019-08-12 16:58:21 -0500

    Multi-threading, Mutex

---
+++ 1114 Print in Order.java
@@ -0,0 +1,40 @@
+/*
+ * @Author: Hao Liu
+ * @Date: 2019-08-12 16:57:34
+ * @LastEditors: Hao Liu
+ * @LastEditTime: 2019-08-12 16:57:46
+ * @Description: 
+ */
+
+import java.util.concurrent.*;
+class Foo {
+    
+    Semaphore m2, m3;
+    
+    public Foo() {
+        m2 = new Semaphore(0);
+        m3 = new Semaphore(0);
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        m2.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        m2.acquire();
+        printSecond.run();
+        m3.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        
+        // printThird.run() outputs "third". Do not change or remove this line.
+        m3.acquire();
+        printThird.run();
+    }
+}

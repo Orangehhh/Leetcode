3f1de1d0e29a27f674fb561e5c96da8f0c422bcd
orangehhh <haoliu47@gmail.com>
2019-10-20 14:23:22 -0500

    Array, two pointers

---
+++ 027 Remove Element.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }   
+        int i = 0;
+        int j = nums.length - 1;
+        while (i <= j) {
+            while (i <= j && nums[i] != val) {
+                i++;
+            }
+            while (i <= j && nums[j] == val) {
+                j--;
+            }
+            if (i < j) {
+                int tmp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = tmp;
+                i++;
+                j--;
+            }
+        }
+        return i;
+    }
+}

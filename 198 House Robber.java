6b4949cfecbaa59409839462375fdb357ff04bd3
orangehhh <haoliu47@gmail.com>
2019-11-21 16:34:53 -0600

    dp

---
+++ 198 House Robber.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        dp[1] = nums[0];
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.max(dp[i - 1], nums[i - 1] + dp[i - 2]);
+        }
+        return dp[n];
+    }
+}

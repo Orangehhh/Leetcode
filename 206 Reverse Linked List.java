9f8ccc30de267719f997d6dabbac116361adcf60
orangehhh <haoliu47@gmail.com>
2020-01-03 14:54:27 -0600

    Iterative

---
+++ 206 Reverse Linked List.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null)   return head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = dummy.next;
+            dummy.next = cur;
+            cur = next; 
+        }
+        head.next = null;
+        return dummy.next;
+    }
+}
